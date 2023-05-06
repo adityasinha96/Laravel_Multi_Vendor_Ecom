b74af6db3133615a000031bd5b2df2fc2417dfda
Ankur Aditya <aditya.ankur@yahoo.com>
2023-05-06 22:42:37 +0530

    Commit Second

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role',['admin','vendor','user'])->default('user'); // Have assigned 3 types of role in which the default role would be user
+            $table->enum('status',['active','inactive'])->default('active'); // Habve made a status table in which there are two option active and inactive byfeault it would be active
             $table->rememberToken();
             $table->timestamps();
         });
