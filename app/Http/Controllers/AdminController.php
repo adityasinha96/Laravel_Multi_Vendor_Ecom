dff5380f5a05d87e0777f20e8f78922ff4e356c6
Ankur Aditya <aditya.ankur@yahoo.com>
2023-05-07 23:16:17 +0530

    Fourth Commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
@@ -8,7 +9,24 @@ class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }
-//----------------------------------------------------------------------------End Method -----------------------------------------------------------------------------------//    
+//----------------------------------------------------------------------------End Method -----------------------------------------------------------------------------------//
+    public function AdminLogin()
+    {
+        return view('admin.admin_login');
+    }
+//----------------------------------------------------------------------------End Method -----------------------------------------------------------------------------------// 
+    
+    public function AdminDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+//----------------------------------------------------------------------------End Method -----------------------------------------------------------------------------------// 
 }
