857e3a155e352c8867032ee415aa241c5d240e53
Ankur Aditya <aditya.ankur@yahoo.com>
2023-05-13 23:18:50 +0530

    Fifth Commit

---
+++ tests/Unit/UserAuthTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Http\Controllers\AdminController;
+
+class UserAuthTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function AdminDestroy()
+    {
+        $this->assertTrue(false);
+    }
+}
